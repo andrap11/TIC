f7bd05cc9abe3685fd34191ca6d7709a0bb11e90
Andrada Popazu <andrada.popazu@qualitance.com>
2018-01-04 16:04:13 +0200

    Small redesign and refactor, add vue to prj

--- app.js
+++ app.js
@@ -1,61 +1,8 @@
-var root = 'https://jsonplaceholder.typicode.com';
-
-$(document).ready(function () {
-
-	$("button").each(function() {
-		this.style.height = '30px';
-		this.style.fontSize = '18px';
-	})
-
-	$("table").addClass("posts-table");
-
-	$('.btn-get').click(function() {
-		getPosts();
-	});
-
-	$('.btn-create').click(function() {
-		var dataObj = {
-			userId: $('.id-field').val(),
-			title: $('.title-field').val(),
-			body: $('.body-field').val(),
-		}
-		createPost(dataObj);
-	});
-
-	function getPosts() {
-		$.ajax({
-			type:"GET",
-			url: root + '/posts',
-			success: function(data) {
-				drawTable(data);
-			},
-			
-		});
-	}
-
-	function createPost(data) {
-		$.ajax({
-			type: "POST",
-			url: root + '/posts',
-			data: {
-				userId: data.userId,
-				title: data.title,
-				body: data.body
-			},
-			success: function(data) {
-				drawTable([data]);
-			},
-			dataType: 'json',
-		});
-	}
-
-	function drawTable(rowData) {
-		for(var i=0; i<rowData.length; i++) {
-			var row = $("<tr />");
-			$(".posts-table").append(row);
-			row.append($("<td>" + rowData[i].id + "</td>"));
-			row.append($("<td>" + rowData[i].title + "</td>"));
-			row.append($("<td>" + rowData[i].body + "</td>"));
-		}
-	}
+document.addEventListener('DOMContentLoaded', function () {
+    new Vue({
+        el: '#vueApp',
+        data: {
+            title: 'E-business Master'
+        }
+    });
 })
