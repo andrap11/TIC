c3def5c58351e1546df5797df4ab67b2ab1290ee
Andrada Popazu <andrada.popazu@qualitance.com>
2018-01-05 21:51:01 +0200

    Refactor using v-for

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ document.addEventListener('DOMContentLoaded', function () {
     new Vue({
         el: '#vueApp',
         data: {
-            title: 'E-business Master'
+            title: 'E-business Master',
+            images: [{
+                name: '',
+                url: 'assets/images/p1.jpg',
+            }, {
+                name: '',
+                url: 'assets/images/p2.jpg',
+            }, {
+                name: '',
+                url: 'assets/images/p3.jpg',
+            }, {
+                name: '',
+                url: 'assets/images/p4.jpg',
+            }, {
+                name: '',
+                url: 'assets/images/p5.jpg',
+            },{
+                name: '',
+                url: 'assets/images/p6.jpg',
+            }]
         }
     });
 })
