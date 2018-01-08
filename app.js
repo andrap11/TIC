bf92825b96887dc0f36e98c24637837bddbc3289
Antal Andrei <antal.andrei@icloud.com>
2018-01-08 13:53:49 +0200

    Add http code

--- app.js
+++ app.js
@@ -3,31 +3,42 @@ document.addEventListener('DOMContentLoaded', function () {
         el: '#vueApp',
         data: {
             title: 'E-business Master',
-            images: [{
-                title: 'Image 1',
-                url: 'assets/images/p1.jpg',
-                isAnimated: true,
-            }, {
-                title: 'Image 2',
-                url: 'assets/images/p2.jpg',
-                isAnimated: false,
-            }, {
-                title: 'Image 3',
-                url: 'assets/images/p3.jpg',
-                isAnimated: false,
-            }, {
-                title: 'Image 4',
-                url: 'assets/images/p4.jpg',
-                isAnimated: true,
-            }, {
-                title: 'Image 5',
-                url: 'assets/images/p5.jpg',
-                isAnimated: true,
-            },{
-                title: 'Image 6',
-                url: 'assets/images/p6.jpg',
-                isAnimated: false,
-            }]
+            // images: [{
+            //     title: 'Image 1',
+            //     url: 'assets/images/p1.jpg',
+            //     isAnimated: true,
+            // }, {
+            //     title: 'Image 2',
+            //     url: 'assets/images/p2.jpg',
+            //     isAnimated: false,
+            // }, {
+            //     title: 'Image 3',
+            //     url: 'assets/images/p3.jpg',
+            //     isAnimated: false,
+            // }, {
+            //     title: 'Image 4',
+            //     url: 'assets/images/p4.jpg',
+            //     isAnimated: true,
+            // }, {
+            //     title: 'Image 5',
+            //     url: 'assets/images/p5.jpg',
+            //     isAnimated: true,
+            // },{
+            //     title: 'Image 6',
+            //     url: 'assets/images/p6.jpg',
+            //     isAnimated: false,
+            // }]
+            images: [],
+        },
+        created: function() {
+            this.$http.get('https://my-json-server.typicode.com/andrei-antal/tic_db/images')
+                .then(function(response) {
+                    this.images = response.body;
+                })
+                .catch(function(error) {
+                    console.log(error)
+                })
+            
         }
     });
 })
