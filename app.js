c241636d7b72e6a6247de9f4457fa03f17e97099
Antal Andrei <antal.andrei@icloud.com>
2018-01-08 14:36:12 +0200

    add configuration options

--- app.js
+++ app.js
@@ -1,37 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     new Vue({
         el: '#vueApp',
+        http: {
+            root: 'https://my-json-server.typicode.com/andrei-antal/tic_db'
+        },
         data: {
             title: 'E-business Master',
-            // images: [{
-            //     title: 'Image 1',
-            //     url: 'assets/images/p1.jpg',
-            //     isAnimated: true,
-            // }, {
-            //     title: 'Image 2',
-            //     url: 'assets/images/p2.jpg',
-            //     isAnimated: false,
-            // }, {
-            //     title: 'Image 3',
-            //     url: 'assets/images/p3.jpg',
-            //     isAnimated: false,
-            // }, {
-            //     title: 'Image 4',
-            //     url: 'assets/images/p4.jpg',
-            //     isAnimated: true,
-            // }, {
-            //     title: 'Image 5',
-            //     url: 'assets/images/p5.jpg',
-            //     isAnimated: true,
-            // },{
-            //     title: 'Image 6',
-            //     url: 'assets/images/p6.jpg',
-            //     isAnimated: false,
-            // }]
             images: [],
         },
         created: function() {
-            this.$http.get('https://my-json-server.typicode.com/andrei-antal/tic_db/images')
+            this.$http.get('images')
                 .then(function(response) {
                     this.images = response.body;
                 })
